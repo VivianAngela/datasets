3475d308e26d76f05dc44e9a534f6b0a05610fb3
Tim Trice <tim.trice@gmail.com>
2017-11-02 08:32:28 -0500

    Drop dir Hurricanes

--- Hurricanes/2016-hurricane-season.R
+++
@@ -1,8 +0,0 @@
-library(Hurricanes)
-
-storms.2016 <- get_storms(2016)
-get_storm_data("fstadv", 
-               link = storms.2016 %>% 
-                 .$Link, 
-               display_link = FALSE)
-write.csv(fstadv, file = "2016-hurricane-season.csv", row.names = FALSE)
