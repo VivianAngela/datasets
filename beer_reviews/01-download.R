9dd88e02b303d917b0c093b4b8efb376ac774a24
Tim Trice <tim.trice@gmail.com>
2018-02-02 13:42:07 -0600

    Add beer_reviews csv

--- beer_reviews/01-download.R
+++ beer_reviews/01-download.R
@@ -2,7 +2,16 @@
 
 #' Data courtesy of Beer Advocate <http://www.beeradvocate.com>
 
+## ---- Libraries ----
+library(DBI)
+
+## ---- Variables ----
+#' IP Address to Postgres database
+ip <- NULL
+
+## ---- Data ----
+
+## ---- * Download ----
 url <- "https://s3.amazonaws.com/demo-datasets/beer_reviews.tar.gz?lipi=urn%3Ali%3Apage%3Ad_flagship3_pulse_read%3B%2BTvuRj%2B2RxyosCECk8fxgQ%3D%3D"
-
-utils::download.file(file.path(url), destfile = "beer_reviews.tar.gz")
-
+utils::download.file(file.path(url), zip_file <- tempfile())
+utils::untar(zip_file, exdir = ".")
